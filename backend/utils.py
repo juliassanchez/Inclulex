147433f049ca36797f8193fa33359fa93b56fd77
Julia Sánchez <98918580+juliassanchez@users.noreply.github.com>
2024-05-29 19:59:33 +0200

    toch last version

--- backend/utils.py
+++ backend/utils.py
@@ -6,7 +6,7 @@ from transformers import (AutoTokenizer,
 def load_nlp_model():
     model_name='stabilityai/stablelm-2-12b'
     bnb_config = BitsAndBytesConfig(
-        #load_in_4bit=True,
+        load_in_4bit=True,
         bnb_4bit_quant_type="nf4",
         bnb_4bit_compute_dtype=torch.float16,
         device_map="auto"
