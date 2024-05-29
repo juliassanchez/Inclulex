d7769393be71337451e8cb5c1d30e6ccd993a6ae
Julia Sánchez <98918580+juliassanchez@users.noreply.github.com>
2024-05-29 20:28:53 +0200

    sin bnb

--- backend/utils.py
+++ backend/utils.py
@@ -5,19 +5,19 @@ from transformers import (AutoTokenizer,
 
 def load_nlp_model():
     model_name='stabilityai/stablelm-2-12b'
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.float16,
-        device_map="auto"
-    )
+    # bnb_config = BitsAndBytesConfig(
+    #     load_in_4bit=True,
+    #     bnb_4bit_quant_type="nf4",
+    #     bnb_4bit_compute_dtype=torch.float16,
+    #     device_map="auto"
+    # )
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     tokenizer.pad_token = tokenizer.eos_token
     model = AutoModelForCausalLM.from_pretrained(
         model_name,
-        device_map="cuda",
-        quantization_config=bnb_config
+        device_map="cuda" #,
+        #quantization_config=bnb_config
     )       
 
     return {
