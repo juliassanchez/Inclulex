321013463002a92b43820d8a4b129f46d157bd26
Julia Sánchez <98918580+juliassanchez@users.noreply.github.com>
2024-06-02 19:46:27 +0200

    intento chat

--- backend/utils.py
+++ backend/utils.py
@@ -3,20 +3,20 @@ from transformers import (AutoTokenizer,
                           AutoModelForCausalLM,
                           BitsAndBytesConfig)
 
-model_name = 'stabilityai/stablelm-2-12b'
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.float16,
-    device_map="auto"
-)
-
 # Variables globales para el modelo y el tokenizador
 nlp_model = None
 tokenizer = None
 
 def load_nlp_model():
     global nlp_model, tokenizer
+
+    model_name = 'stabilityai/stablelm-2-12b'
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.float16,
+        device_map="auto"
+    )
     
     tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir='/app/backend/cache')
     tokenizer.pad_token = tokenizer.eos_token
