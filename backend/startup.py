a192f00d2dd5c33bda35b4f381bba12fe3e3b1a6
Julia Sánchez <98918580+juliassanchez@users.noreply.github.com>
2024-06-03 18:12:16 +0200

    prueba sin nlp model

--- backend/startup.py
+++ backend/startup.py
@@ -14,6 +14,6 @@ print('compile-spanish function executed successfully.')
 # Cargar el tokenizador y el modelo pre-entrenado de GPT-2 en español
 print('Loading NLP model...')
 
-load_nlp_model()
+print('ok')
 
 print('NLP model loaded successfully.')
